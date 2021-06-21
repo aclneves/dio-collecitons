5858581434ad3ee9dca5de588722dfdd74ad0ee6
aclneves <aclneves@gmail.com>
2021-06-21 11:40:21 -0300

    Criação dos exemplos do Stream e resolução do exercicio 3 do Maps

---
+++ dio-collections/src/br/com/antonio/neves/streamsAPI/ExerciciosStreamAPI.java
@@ -0,0 +1,12 @@
+package br.com.antonio.neves.streamsAPI;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ExerciciosStreamAPI {
+    public static void main(String[] args) {
+        List<String> numerosAleatorios =
+                Arrays.asList("1", "0", "4", "1", "2", "3", "9", "6", "5");
+
+    }
+}
